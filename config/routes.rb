e3400090d503d045f619b6fd8464d9dd7e41e5e1
Juan Ignacio Sanz <jsanz.eps@ceu.es>
2019-04-02 08:48:48 +0000

    Rutas de landing y home creadas

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root 'home#landing'
+  get '/contacto' => 'home#contact'
+  get '/acerca_de' => 'home#about'
+  get '/quienes_somos' => 'home#who'
+  get '/donde_estamos' => 'home#where'
   get 'legal/privacidad' => 'legal#privacy'
   get 'legal/cookies'
   get 'legal/terminos_de_uso' => 'legal#terms'
